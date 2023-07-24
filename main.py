514b3a90ad31c9bf564e5b93902edcf538a8f837
André Ohuti <ohuti@hotmail.com>
2023-07-24 19:50:14 -0300

    counting cells

--- main.py
+++ main.py
@@ -4,57 +4,98 @@ import cv2 as cv
 import numpy as np
 
 
-WHITE_THRESHOLD = 185
+DIR_PATH = os.environ['DIR_PATH']
+WHITE_THRESHOLD = int(os.environ['WHITE_THRESHOLD'])
+CHECK_PARALLEL_PIXELS = int(os.environ['CHECK_PARALLEL_PIXELS'])
 
 
-def crop_outside_three_lines(gray_img):
-    col = math.floor(gray_img.shape[1]/3)
+def crop_outside_three_lines(img):
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    col = math.floor(gray_img.shape[1]/4)
     y1_threshold = None
     for row in range(gray_img.shape[0]):
-        if gray_img[row][col] > WHITE_THRESHOLD:
-            y1_threshold = row
-        elif y1_threshold is not None and gray_img[row][col] < WHITE_THRESHOLD and row - y1_threshold > 50:
+        for index in range(CHECK_PARALLEL_PIXELS):
+            if gray_img[row][col + index] > WHITE_THRESHOLD:
+                y1_threshold = row
+                break
+            if index == 0:
+                continue
+            elif gray_img[row][col - index] > WHITE_THRESHOLD:
+                y1_threshold = row
+                break
+        if y1_threshold is not None and row - y1_threshold > 50:
             break
+
 
     row = math.floor(gray_img.shape[0]/2)
     x1_threshold = None
     for col in range(gray_img.shape[1]):
-        if gray_img[row][col] > WHITE_THRESHOLD:
-            x1_threshold = col
-        elif x1_threshold is not None and gray_img[row][col] < WHITE_THRESHOLD and col - x1_threshold > 50:
+        for index in range(CHECK_PARALLEL_PIXELS):
+            if gray_img[row + index][col] > WHITE_THRESHOLD:
+                x1_threshold = col
+                break
+            if index == 0:
+                continue
+            elif gray_img[row - index][col] > WHITE_THRESHOLD:
+                x1_threshold = col
+                break
+        if x1_threshold is not None and col - x1_threshold > 50:
             break
 
     x2_threshold = None
     for col in range(gray_img.shape[1]):
         true_col = gray_img.shape[1] - col - 1
-        if gray_img[row][true_col] > WHITE_THRESHOLD:
-            x2_threshold = true_col
-        elif x2_threshold is not None and gray_img[row][true_col] < WHITE_THRESHOLD and x2_threshold - true_col > 50:
+        for index in range(CHECK_PARALLEL_PIXELS):
+            if gray_img[row + index][true_col] > WHITE_THRESHOLD:
+                x2_threshold = true_col
+                break
+            if index == 0:
+                    continue
+            elif gray_img[row - index][true_col] > WHITE_THRESHOLD:
+                x2_threshold = true_col
+                break
+        if x2_threshold is not None and x2_threshold - true_col > 50:
             break
 
-    blank = np.zeros(gray_img.shape[:2], dtype=np.uint8)
-    mask = cv.rectangle(blank, (x1_threshold, y1_threshold), (x2_threshold, gray_img.shape[0]), 255, -1)
+    return img[y1_threshold:gray_img.shape[0], x1_threshold:x2_threshold], gray_img[y1_threshold:gray_img.shape[0], x1_threshold:x2_threshold]
 
-    print(f'x1_threshold: {x1_threshold}')
-    print(f'x2_threshold: {x2_threshold}')
-    print(f'y1_threshold: {y1_threshold}')
-    
-    return cv.bitwise_and(gray_img, mask)
+def mask_subsections_and_count_cells(gray_img):
+    gray_copy = gray_img.copy()
+    blured_img = cv.GaussianBlur(gray_img, (7,7), 0)
+
+    _, thresh = cv.threshold(blured_img, 115, 255, cv.THRESH_BINARY)
+    cv.imshow('thresh', thresh)
+
+    contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    cv.drawContours(gray_copy, contours, -1, (0, 255, 0), 2)
+    cv.imshow('contours', gray_copy)
+
+    rounded_contours = []
+    for contour in contours:
+        approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
+
+        if len(approx) < 3:
+            rounded_contours.append(contour)
+
+    return rounded_contours
 
 def main():
-    files = os.listdir('images/Animal 20/Eritrócito')
-
+    files = os.listdir(DIR_PATH)
     for file in files:
-        path = os.path.join('images/Animal 20/Eritrócito', file)
+        path = os.path.join(DIR_PATH, file)
         img = cv.imread(path)
 
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
         print(f'handling file {path}')
-        masked_gray = crop_outside_three_lines(gray)
-        cv.imshow(f'grayscale', gray)
-        cv.imshow(f'masked image', masked_gray)
+        masked, masked_gray = crop_outside_three_lines(img)
+        contours = mask_subsections_and_count_cells(masked_gray)
+        print(f'Contours detected: {len(contours)}')
+
+        cv.drawContours(masked, contours, -1, (0, 255, 0), 5)
+
+        cv.imshow('result', masked)
         cv.waitKey(0)
+        break
 
     # cv2.imshow('gray', gray)
 
